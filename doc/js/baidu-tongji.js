066f733723cb29c4d6168d56c02730164199b2a9
sixfly <c2ccc2@qq.com>
2022-11-29 16:23:59 +0800

    push doc

---
+++ doc/js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6a2233305903823f306c6273f36b399d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
