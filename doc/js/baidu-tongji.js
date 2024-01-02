4a8d8cc5802aaca1c6443dd8ce011182bdb30845
cc <c2ccc2@qq.com>
2024-01-02 11:52:03 +0800

    增加doc目录

--- doc/js/baidu-tongji.js
+++
@@ -1,9 +0,0 @@
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?6a2233305903823f306c6273f36b399d";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
